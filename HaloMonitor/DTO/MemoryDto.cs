d3f650c85c0c4d8443577d46e18489a3f73ef915
WuYangBing <2394836871@qq.com>
2026-01-08 16:12:47 +0800

    项目结束添加项目文件。

---
+++ HaloMonitor/DTO/MemoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HaloMonitor.DTO
+{
+    internal class MemoryDto
+    {
+        public string? MemoryInfo { get; set; }
+        public string? MemoryLoad { get; set; }
+        public string? Used { get; set; }
+        public string? Free { get; set; }
+    }
+}
---
+++ HaloMonitor/DTO/StorageDto.cs
@@ -0,0 +1,20 @@
+﻿
+namespace HaloMonitor.DTO
+{
+    internal class StorageDto
+    {
+        public string DiskInfo { get; set; } = string.Empty;
+
+        public string? Temp { get; set; }
+
+        public string? UsedPercent { get; set; }
+
+        public string? Read { get; set; }
+
+        public string? Written { get; set; }
+
+        public string? ReadSpeed { get; set; }
+
+        public string? WriteSpeed { get; set; }
+    }
+}
